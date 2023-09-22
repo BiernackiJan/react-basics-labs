4fe6691c70961cf3ecc9bdb44fc8d19f1f64361c
Jan <janbier2000@gmail.com>
2023-09-22 15:51:09 +0100

    Completed Week 2 Lab

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import Task from './components/Tasks';
+import './App.css';
+import React, { useState } from 'react';
+
+function App() {
+  const [ taskState, setTaskState ] = useState({
+    tasks: [
+      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
+      { id: 3, title: "Tidy up", deadline: "Today" },
+      { id: 4, title: "Giveaway", description: "Get a giveaway for Esports", deadline: "Next Week"},
+      { id: 5, title: "Societies Day", description: "Prepare for societies day" , deadline: "Wednesday"}
+    ]
+  });
+  return (
+    <div className="container">
+      <h1>Tasky</h1>
+      {taskState.tasks.map((task) => (              
+        <Task 
+          title={task.title}
+          description={task.description}
+          deadline={task.deadline}
+          key={task.id}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default App;
